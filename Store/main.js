18c43d1c0d677962ffe44d603699d355f61562c5
zahid-hossain34 <zahed15-9395@diu.edu.bd>
2021-03-09 04:27:37 -0800

    first commit

---
+++ Store/main.js
@@ -0,0 +1,13 @@
+/* const toggleBtn =document.querySelector('#toggleBtn');
+const divList =document.querySelector('.cheak');
+
+toggleBtn.addEventListener('click' , () => {
+    if(divList.style.display === 'none'){
+        divList.style.display = 'block';
+    }
+    else{
+        divList.style.display = 'none';
+    }
+
+}); */
+
